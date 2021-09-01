73a8eaa3c92eeb47e370e6681d2fb0b31777391a
Seraphina4 <negi.swati21@outlook.com>
2021-09-01 14:13:12 +0530

    Part 3: Sloution

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -3,12 +3,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class RestaurantTest {
     Restaurant restaurant;
+
+    //    TestMenu is used as the menu used by user to select items
+    List<Item> TestMenu = new ArrayList<Item>();
     //REFACTOR ALL THE REPEATED LINES OF CODE
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -16,11 +21,7 @@ class RestaurantTest {
 
     @Test
     public boolean is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
+        RestaurantCafe();
 
         return true;
 
@@ -39,23 +40,24 @@ class RestaurantTest {
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void adding_item_to_menu_should_increase_menu_size_by_1(){
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
+        RestaurantCafe();
 
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.addToMenu("Sizzling brownie",319);
         assertEquals(initialMenuSize+1,restaurant.getMenu().size());
     }
-    @Test
-    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+
+    private void RestaurantCafe() {
         LocalTime openingTime = LocalTime.parse("10:30:00");
         LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
+        restaurant.addToMenu("Sweet corn soup", 119);
         restaurant.addToMenu("Vegetable lasagne", 269);
+    }
+
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+        RestaurantCafe();
 
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.removeFromMenu("Vegetable lasagne");
@@ -63,14 +65,37 @@ class RestaurantTest {
     }
     @Test
     public void removing_item_that_does_not_exist_should_throw_exception() {
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
+        RestaurantCafe();
 
         assertThrows(itemNotFoundException.class,
                 ()->restaurant.removeFromMenu("French fries"));
     }
+
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @Test
+    public void order_value_must_sum_up_when_multiple_items_are_selected (){
+        createTestRestaurant();
+
+        TestMenu = restaurant.getMenu();
+
+        assertEquals(388,restaurant.getOrderValue(dummyMenu));
+
+    }
+
+
+    @Test
+    public void order_value_must_reduce_when_item_is_removed (){
+        createTestRestaurant();
+
+        TestMenu = restaurant.getMenu();
+        int total = restaurant.getOrderValue(dummyMenu);
+        int priceOfItemRemoved = dummyMenu.get(1).getPrice();
+        dummyMenu.remove(1);
+
+
+
+        assertEquals(total - priceOfItemRemoved,restaurant.getOrderValue(dummyMenu));
+
+    }
 }
+
